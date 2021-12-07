106f1e96cf3d9c1fc36a414d96762133ad73016e
clank <clank@gmail.com>
2021-12-07 22:27:45 +0800

    调整api域名

--- vue.config.js
+++ vue.config.js
@@ -35,7 +35,7 @@ module.exports = {
         // 代理重写
         proxy: {
             '/Api': {
-                target: 'http://rubikcubexy.club', // target表示代理的服务器url
+                target: 'http://qianlima.rubikcubexy.club', // target表示代理的服务器url
                 /*pathRewrite: { // pathRewrite表示路径重写，key表示一个正则，value表示别名
                     '^/api': '/' // 即用 '/api'表示'http://localhost:3000/api'
                 }*/
